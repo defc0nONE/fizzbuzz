7f43376be6db2940d3c1e219bc902e7c1888207d
Christopher Ly <demedial@gmail.com>
2018-02-14 23:01:04 +0000

    Refactored code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,16 +1,13 @@
 def fizzbuzz(x)
   if x.is_a? Integer
     if x % 3 == 0 && x % 5 == 0
-      return 'fizzbuzz'
+      'fizzbuzz'
     elsif x % 3 == 0
-      return 'fizz'
+      'fizz'
     elsif x % 5 == 0
-      return 'buzz'
-    else
-      return x
+      'buzz'
+    else x
     end
-
-  else
-    'This is not a number'
+  else 'This is not a number'
   end
 end
