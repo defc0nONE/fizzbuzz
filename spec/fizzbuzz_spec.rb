96426ff8a650331736ce2d705682f1d7afac8409
Christopher Ly <demedial@gmail.com>
2018-02-14 22:10:36 +0000

    Created more tests and an edge case, coded the program to spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'converts 3 to Fizz' do
+  it "converts 3 to Fizz" do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it "doesn't change 4" do
+    expect(fizzbuzz(4)).to eq 4
+  end
+end
+
+describe 'fizzbuzz' do
+  it "converts 5 to fizzbuzz" do
+    expect(fizzbuzz(5)).to eq 'fizzbuzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it "cannot take a string" do
+    expect(fizzbuzz('string')).to eq 'This is not a number'
   end
 end
